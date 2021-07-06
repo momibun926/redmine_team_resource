f0683258d51612122cf4dc2dd771ad0b43e3aec7
Hajime Nakagama <hajime.nakagama@gmail.com>
2021-07-06 20:58:00 +0900

    work in progress

--- app/helpers/teamresources_helper.rb
+++ app/helpers/teamresources_helper.rb
@@ -1,3 +1,10 @@
 # Team resource helper.
 module TeamresourcesHelper
+  def summary_title(before_key, current_key)
+    if before_key == current_key
+      current_key.to_s.split("USER")[1].split("PROJECT")[1]
+    else
+      current_key.to_s.split("USER")[1].split("PROJECT")[0]
+    end
+  end
 end
