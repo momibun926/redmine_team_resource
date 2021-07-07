02df2c3ff88e7237161aead82e9dcbabbc136c46
Hajime Nakagama <hajime.nakagama@gmail.com>
2021-07-07 16:11:58 +0900

    work in progress

--- app/controllers/teamresources_controller.rb
+++ app/controllers/teamresources_controller.rb
@@ -5,10 +5,10 @@ class TeamresourcesController < ApplicationController
   # View of main page.
   #
   def index
+    @selectable_project_list = Project.all
     @selected_project_ids = params[:selected_project_ids] || {}
     summarize_by_date = create_summarize_by_date(@selected_project_ids)
     @summarize_by_month = create_summarize_by_month(summarize_by_date)
     @reange_month = create_range_month(@summarize_by_month)
-    @selectable_project_list = Project.all
   end
 end
