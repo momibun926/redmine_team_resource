407dbcd4d10b97a10b54228702b2629a5b613fce
Hajime Nakagama <hajime.nakagama@gmail.com>
2021-07-07 13:39:32 +0900

    work in progress

--- app/controllers/teamresources_controller.rb
+++ app/controllers/teamresources_controller.rb
@@ -4,7 +4,7 @@
 # 1. before action
 #
 class TeamresourcesController < ApplicationController
-  include ResourceDataFetcher
+  include SummarizeData
   # menu
   # menu_item :teamresourcemonitor
   # Before action
@@ -15,72 +15,8 @@ class TeamresourcesController < ApplicationController
   def index
     # test code
     project_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    summarize_by_date = {}
-    issues = specify_project_issue(project_ids)
-    issues.each do |issue|
-      issue.due_date ||= Version.find(issue.fixed_version_id).effective_date
-      issue.assigned_to_id ||= 0
-      assigned_days = working_days(issue.start_date, issue.due_date)
-      hours_per_day = issue_hours_per_day(issue.estimated_hours.to_f, assigned_days.length)
-      hash_key = "USER#{issue.assigned_to_id}PROJECT#{issue.project_id}".to_sym
-      temp = summarize_by_date[hash_key]
-      temp ||= {}
-      assigned_days.each do |date|
-        temp[date] = add_daily_hour(temp[date], hours_per_day)
-      end
-      summarize_by_date[hash_key] = temp
-    end
-
-    temp_days = {}
-    @range_start_month = nil
-    @range_end_month = nil
-    @summarize_by_month = {}
-    range_year_month = []
-    summarize_by_date.each_key do |k|
-      temp_days = summarize_by_date[k]
-      temp_month = {}
-      temp_days.each do |k, v|
-        if temp_month[k.strftime("%Y-%m")].nil?
-          temp_month[k.strftime("%Y-%m")] = temp_days[k]
-        else
-          temp_month[k.strftime("%Y-%m")] += temp_days[k]
-        end
-        range_year_month << k.strftime("%Y-%m")
-      end
-      @summarize_by_month[k] = temp_month
-    end
-    @reange_month = range_year_month.uniq.sort
-  end
-
-  # Estimated time per day.
-  #
-  # @param [Numeric] estimated_hours estimated hours
-  # @param [Numeric] days working days
-  # @return [Numeric] estimate hours per days
-  def issue_hours_per_day(estimated_hours, days)
-    (estimated_hours || 0.0) / days
-  end
-
-  # working days.
-  # exclude weekends and holiday or include weekends and holiday.
-  #
-  # @param [Date] start_date start date of issue
-  # @param [Date] end_date end date of issue
-  # @return [Array] working days
-  def working_days(start_date, end_date)
-    issue_days = (start_date..end_date).to_a
-    working_days = issue_days.reject { |e| e.wday.zero? || e.wday == 6 }
-    working_days.length.zero? ? issue_days : working_days
-  end
-
-  # Add daily hour
-  #
-  # @param [Numeric] evm_value EVM value
-  # @param [Numeric] value EVM value
-  # @param [Numeric] done_ratio done ratio
-  # @return [Numeric] after add value
-  def add_daily_hour(evm_value, value, done_ratio = 100)
-    add_value = value.to_f * done_ratio.fdiv(100)
-    evm_value.nil? ? add_value : evm_value.to_f + add_value
+    summarize_by_date = create_summarize_by_date(project_ids)
+    @summarize_by_month = create_summarize_by_month(summarize_by_date)
+    @reange_month = create_range_month(@summarize_by_month)
   end
 end
---
+++ lib/summarize_data.rb
@@ -0,0 +1,84 @@
+# Summarize data
+#
+module SummarizeData
+  include ResourceDataFetcher
+  # Summarize by date
+  def create_summarize_by_date(project_ids)
+    summarize_by_date = {}
+    issues = specify_project_issue(project_ids)
+    issues.each do |issue|
+      issue.due_date ||= Version.find(issue.fixed_version_id).effective_date
+      issue.assigned_to_id ||= 0
+      assigned_days = working_days(issue.start_date, issue.due_date)
+      hours_per_day = issue_hours_per_day(issue.estimated_hours.to_f, assigned_days.length)
+      hash_key = "USER#{issue.assigned_to_id}PROJECT#{issue.project_id}".to_sym
+      temp = summarize_by_date[hash_key]
+      temp ||= {}
+      assigned_days.each do |date|
+        temp[date] = add_daily_hour(temp[date], hours_per_day)
+      end
+      summarize_by_date[hash_key] = temp
+    end
+    summarize_by_date
+  end
+
+  # Create summarize date by month
+  def create_summarize_by_month(summarize_by_date)
+    temp_days = {}
+    summarize_by_month = {}
+    summarize_by_date.each_key do |k|
+      temp_days = summarize_by_date[k]
+      temp_month = {}
+      temp_days.each do |k, v|
+        if temp_month[k.strftime("%Y-%m")].nil?
+          temp_month[k.strftime("%Y-%m")] = temp_days[k]
+        else
+          temp_month[k.strftime("%Y-%m")] += temp_days[k]
+        end
+      end
+      summarize_by_month[k] = temp_month
+    end
+    summarize_by_month
+  end
+
+  # create range of month
+  def create_range_month(summarize_by_month)
+    month_array = []
+    summarize_by_month.each_key do |k|
+      month_array << summarize_by_month[k].keys.uniq
+    end
+    month_array.flatten.uniq.sort
+  end
+
+  # Estimated time per day.
+  #
+  # @param [Numeric] estimated_hours estimated hours
+  # @param [Numeric] days working days
+  # @return [Numeric] estimate hours per days
+  def issue_hours_per_day(estimated_hours, days)
+    (estimated_hours || 0.0) / days
+  end
+
+  # working days.
+  # exclude weekends and holiday or include weekends and holiday.
+  #
+  # @param [Date] start_date start date of issue
+  # @param [Date] end_date end date of issue
+  # @return [Array] working days
+  def working_days(start_date, end_date)
+    issue_days = (start_date..end_date).to_a
+    working_days = issue_days.reject { |e| e.wday.zero? || e.wday == 6 }
+    working_days.length.zero? ? issue_days : working_days
+  end
+
+  # Add daily hour
+  #
+  # @param [Numeric] evm_value EVM value
+  # @param [Numeric] value EVM value
+  # @param [Numeric] done_ratio done ratio
+  # @return [Numeric] after add value
+  def add_daily_hour(evm_value, value, done_ratio = 100)
+    add_value = value.to_f * done_ratio.fdiv(100)
+    evm_value.nil? ? add_value : evm_value.to_f + add_value
+  end
+end
