d79411232af8454c18476426d0447820e876d6e2
Hajime Nakagama <hajime.nakagama@gmail.com>
2021-07-08 09:17:43 +0900

    l18

--- app/helpers/teamresources_helper.rb
+++ app/helpers/teamresources_helper.rb
@@ -6,7 +6,7 @@ module TeamresourcesHelper
   # @param [Symbol] key concatinate user id and project id. ex: USER1PROJECT10
   # @return [String] user name.
   def get_user_name(key)
-    User.find_by(id: key.to_s.split("USER")[1].split("PROJECT")[0]) || "Not Assigned"
+    User.find_by(id: key.to_s.split("USER")[1].split("PROJECT")[0]) || l(:no_assigned)
   end
 
   # Get project name from hash key
