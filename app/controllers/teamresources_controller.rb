06003416e497dbf51c8df1333be1c833a3d4fe22
Hajime Nakagama <hajime.nakagama@gmail.com>
2021-07-08 17:22:39 +0900

    Small refactoring

--- app/controllers/teamresources_controller.rb
+++ app/controllers/teamresources_controller.rb
@@ -10,9 +10,9 @@ class TeamresourcesController < ApplicationController
                                                   :view_project,
                                                   { member: true }).active
     # selected project ids
-    @selected_project_ids = params[:selected_project_ids] || @selectable_project_list
+    selected_project_ids = params[:selected_project_ids] || @selectable_project_list
     # summarize data
-    summarize_by_date = create_summarize_by_date(@selected_project_ids)
+    summarize_by_date = create_summarize_by_date(selected_project_ids)
     @summarize_by_month = create_summarize_by_month(summarize_by_date)
     @summarize_user_total_by_month = create_summarize_user_total_by_month(@summarize_by_month)
     @range_month = create_range_month(@summarize_by_month)
@@ -26,10 +26,6 @@ class TeamresourcesController < ApplicationController
   # No set hours of day is "1.0"
   #
   def default_hours_of_day
-    if params[:hours_of_day].nil? || params[:hours_of_day] == ""
-      "1.0"
-    else
-      params[:hours_of_day]
-    end
+    params[:hours_of_day].presence || "1.0"
   end
 end
