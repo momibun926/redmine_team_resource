9b5504407787afc08406fb1b015e6e6a5bd11dd9
Hajime Nakagama <hajime.nakagama@gmail.com>
2021-07-08 15:20:07 +0900

    Bug fix

--- app/controllers/teamresources_controller.rb
+++ app/controllers/teamresources_controller.rb
@@ -15,6 +15,19 @@ class TeamresourcesController < ApplicationController
     @summarize_user_total_by_month = create_summarize_user_total_by_month(@summarize_by_month)
     @range_month = create_range_month(@summarize_by_month)
     # hours of day
-    @hours_of_day = params[:hours_of_day] || "1.0"
+    @hours_of_day = default_hours_of_day
+  end
+
+  private
+
+  # Default hours of day
+  # No set hours of day is "1.0"
+  #
+  def default_hours_of_day
+    if params[:hours_of_day].nil? or params[:hours_of_day] == ""
+      "1.0"
+    else
+      params[:hours_of_day]
+    end
   end
 end
